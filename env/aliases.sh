26dd355c1d9afb088bfd1646e6ab25e76d8df1d4
Jason Hinchcliffe <hinchy@gmail.com>
2020-08-13 16:23:11 +1000

    BALEETED. and a minimal fresh install

---
+++ env/aliases.sh
@@ -0,0 +1,13 @@
+# Aliases
+echo "Adding aliases"
+# Work specific
+alias gotomp="cd ~/Sites/marketplacer"
+alias gotoint="cd ~/Sites/integrations"
+alias gotoket="cd ~/Sites/ketplacer"
+alias vimsetup="nvim ~/.config/nvim/init.vim"
+
+# Ruby specific
+alias be="bundle exec"
+
+# test
+alias xxx="echo poopxxxsss"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-echo "Copying .bash_profile to home directory"
-cp .bash_profile ~
-echo "Copying .gitconfig to home directory"
-cp .git_config ~
+
+#!/bin/bash
+#source ~/.fresh/build/shell.sh
+echo "Giddy up!"
+# Removing local .zshrc, installing Fresh, and then linking .freshrc to my prebaked one
+rm ~/.zshrc
+bash -c "`curl -sL get.freshshell.com`"
+# rm ~/.freshrc
+ln -s ~/dotfiles/.freshrc ~/.freshrc
+
+fresh env/aliases.sh
